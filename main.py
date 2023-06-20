2e9c1770294f7fff9fb21908063253a0063f4979
adubig24 <132444910+adubig24@users.noreply.github.com>
2023-06-20 16:42:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ import streamlit.web.bootstrap as stb
 import os
 
 def main():
-    df=dd.get_data(dq.queryst1)
+    # Récupérer les données
+    df = dd.get_data(dq.queryst1)
+
+    # Sélectionner une usine avec une boîte de sélection
     option = st.selectbox('Choisissez votre usine', df['usine'].unique())
+
+    # Afficher les données filtrées selon l'usine sélectionnée
     st.dataframe(df[df['usine'] == option].reset_index(drop=True).drop('usine', axis=1))
 
+    # Afficher les graphiques
     dg.graph1()
     dg.graph2()
 
@@ -17,6 +23,7 @@ if __name__ == "__main__":
     if st.runtime.exists():
         main()
     else:
+        # Exécuter l'application Streamlit avec Streamlit-Boostrap
         streamlit_script_path = os.path.join(os.getcwd(), "main.py")
-        stb.run(streamlit_script_path,'',[],flag_options=[])
+        stb.run(streamlit_script_path, '', [], flag_options=[])
         main()
